d01276a12ec604e2c420d74a32d9efc7c817c700
ThePope05 <swobben05@gmail.com>
2023-12-21 08:15:32 +0100

    Fixed component error

--- app/require.php
+++ app/require.php
@@ -4,11 +4,4 @@ require_once 'libraries/BaseController.php';
 require_once 'libraries/Database.php';
 require_once 'config/config.php';
 
-$components = scandir('./components');
-foreach ($components as $component) {
-    if ($component != '.' && $component != '..') {
-        require_once './components/' . $component;
-    }
-}
-
 $init = new Core();
