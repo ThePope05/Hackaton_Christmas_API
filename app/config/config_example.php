c050381c320d687a28f0538ad9aa85b8729aec6c
ThePope05 <swobben05@gmail.com>
2023-12-21 08:02:16 +0100

    New mvc framework

---
+++ app/config/config_example.php
@@ -0,0 +1,9 @@
+<?php
+
+//Remove _example from the file name and fill in the details below
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('URLROOT', '');
